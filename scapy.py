e398e1eb052c8657599ea31adc22ceb40e696e2e
sansna <1185280650@qq.com>
2017-05-08 06:47:17 +0000

    master added usage template of scapy

---
+++ scapy.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python2
+
+from scapy.all import * # Changing from scapy to scapy.all
+# Ways to get Doc of scapy lib.
+help(scapy)
+help(scapy.all)
+help(scapy.sendrecv)
+
+pkts=sniff(count=1,filter="arp")
+arppkt=eval(pkts[0].command())
+# ls(Ether) # Show args of array Ether..
+arppkt[Ether].dst="68:05:ca:3f:6d:e1"
+# ls(ARP) # Show args of array ARP..
+arppkt[ARP].op=2 #Reply, 1 if request
+arppkt[hwdst]="68:05:ca:3f:6d:e1"
+ifaces
+#send(arppkt,iface="ens2f0") #In windows, using send(), seems in linux, send()
+#+ do not have arg iface.. It said send() is used to send layer 3 pkts. and
+#+ sendp is used to send layer 2 pkts.
+sendp(arppkt,iface="ens2f0") #In linux, using sendp()..
