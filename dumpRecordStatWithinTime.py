cb80bad077d9c0d70b2a368bda2ffaba3dd20270
sansna <1185280650@qq.com>
2018-11-14 17:40:07 +0800

    adds pymongo usage

---
+++ dumpRecordStatWithinTime.py
@@ -0,0 +1,28 @@
+#!/usr/local/bin/python3
+import sys
+import pymongo
+import argparse
+
+p = argparse.ArgumentParser(conflict_handler='resolve')
+p.add_argument("-h", type=str, default='172.16.2.157', help='mgodb host')
+p.add_argument("-p", type=int, default=27047, help='mgodb port')
+p.add_argument("-c", type=int, default=0, help='after ct')
+p.add_argument("-u", type=int, default=0, help='after ut')
+p.add_argument("-n", type=int, default=None, help='dump limit')
+args = p.parse_args()
+
+conn = pymongo.MongoClient(host=args.h, port=args.p, read_preference=pymongo.read_preferences.ReadPreference.SECONDARY_PREFERRED)
+
+t = conn['record']['record_square']
+
+cnt = 0
+for i in t.find({"$and":[{"ut":{"$gt":args.u}},{"ct":{"$gt":args.c}}]}):
+    t.find
+    if args.n != None and cnt >= args.n:
+        break
+    cnt += 1
+    if cnt % 1000 == 0:
+        sys.stderr.write('lines: %d\n' % cnt)
+
+    print(i.get('_id', 0), i.get('mid', 0), i.get('tid', 0), i.get('ct', 0), i.get('ut', 0), i.get('status', 0), i.get('score', 0.0), i.get('targer_disp_count', 0))
+    sys.stdout.flush()
