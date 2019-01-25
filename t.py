6dba61844fc06f39ae16660364acc04c515bf845
sansna <1185280650@qq.com>
2019-01-25 14:26:47 +0800

    from date to unix timestsamp

--- t.py
+++ t.py
@@ -1,5 +1,9 @@
 # Note: do not name this file as time.py
 # Otherwise the following import will import itself
 import time
+import datetime
 print(time.strftime("%Y-%m-%d",time.localtime(time.time()-24*60*60)))
 print(time.strftime("%Y%m%d,%H:%M:%S",time.localtime(time.time())))
+a = datetime.date(2018,1,1)
+j = time.mktime(a.timetuple())
+print int(j)
