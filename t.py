7b7fee89d6b35bcf93b8a9d884f16b2ea43e5ff3
sansna <1185280650@qq.com>
2019-01-25 14:33:36 +0800

    update time

--- t.py
+++ t.py
@@ -2,8 +2,21 @@
 # Otherwise the following import will import itself
 import time
 import datetime
+# date to string
 print(time.strftime("%Y-%m-%d",time.localtime(time.time()-24*60*60)))
 print(time.strftime("%Y%m%d,%H:%M:%S",time.localtime(time.time())))
+
+# date to timestamp
 a = datetime.date(2018,1,1)
 j = time.mktime(a.timetuple())
 print int(j)
+
+# timestamp to string
+ts = 1172969203
+c = time.ctime(ts)
+print c
+
+# timestamp to date obj
+ts = 1172969203
+d = datetime.datetime.fromtimestamp(ts)
+print d.year, d.month, d.day
