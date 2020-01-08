689cf77238d31c06528ecfaf5a7e22cd9afda884
sansna <1185280650@qq.com>
2020-01-08 23:31:30 +0800

    @Wentao[master]: update more actions - sansna

--- argp.py
+++ argp.py
@@ -1,4 +1,4 @@
-# Run this as: python argp.py 999 -m df -m 12 -d 123 23 -i 12 -d xc ksdf --new asdf
+# Run this as: python argp.py 999 -m df -m 12 -d 123 23 -i 12 -d xc ksdf -+new asdf -r sz
 #+ Usage of argparse: https://docs.python.org/3/library/argparse.html
 import argparse
 
@@ -8,6 +8,10 @@ p.add_argument("-p", "--hel", "--help-menu", type=str, default="host", help="sdf
 p.add_argument("-hm", type=str, default="lsdf", help="sdfsdf")
 # 1 dim list: [], add args by specify another "-m"
 p.add_argument("-m", action="append")
+# 1 dim array, collisionable
+p.add_argument("-n", nargs="+")
+# another 1 dim array. extends current array each time specifies, argparse version should >=3.8
+#p.add_argument("-e", action="extend")
 # 2 dim list: [[], []], each "-d" specifies a list, separated by space.
 #+ nargs values: '?', '*', 'N', '+'
 p.add_argument("-d", action="append", nargs="*")
@@ -17,6 +21,8 @@ p.add_argument("-i", type=int)
 p.add_argument("-c", choices=["left", "right", "up", "down"])
 # default var
 p.add_argument("level", type=int)
+# required, also note the first option name is used as destination variable name.
+p.add_argument("--required","-r", required=True)
 
 # Parent/Child usage, newp has all of p's arguments, and rename to WOW
 newp = argparse.ArgumentParser(parents=[p], prog="WOW", prefix_chars="-+")
@@ -43,3 +49,6 @@ print "type specified:",type(args.i)
 print "choices: ",args.c
 print "first var: %s"%args.level
 print "newp:%s"%args.new
+print "required:%s"%args.required
+#print "extend: %s"%args.e
+print "nargs: %s"%args.n
