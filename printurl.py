6d129f7d0905a4317330fef20d7b239106ea71ae
sansna <1185280650@qq.com>
2020-03-08 23:23:31 +0800

    @Wentao[master]: pudat - sansna

--- printurl.py
+++ printurl.py
@@ -1,4 +1,4 @@
-# Usage: mitmproxy -q -s printurl.py -r dumpfile
+# Usage: mitmdump -q -s printurl.py -r dumpfile
 from mitmproxy import http
 def request(flow: http.HTTPFlow):
     print (flow.request.url)
