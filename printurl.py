a6f735dd61d040bcb31cd5d5a919c890d1142582
sansna <1185280650@qq.com>
2020-04-05 17:44:45 +0800

    @Wentao[master]: savefile to curl  - sansna

--- printurl.py
+++ printurl.py
@@ -1,4 +1,6 @@
 # Usage: mitmdump -q -s printurl.py -r dumpfile
 from mitmproxy import http
 def request(flow: http.HTTPFlow):
-    print (flow.request.url)
+    if len(flow.request.content) > 0:
+        #flow.request.content is of type bytes
+        print ("curl -d '%s' %s?sign="%(str(flow.request.content, "utf-8"), str(flow.request.url).split("?")[0]))
