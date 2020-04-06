f31abd90f2df78c218fb4af113e59c5180310db3
sansna <1185280650@qq.com>
2020-04-06 17:41:40 +0800

    @Wentao[master]: update - sansna

--- rapi.py
+++ rapi.py
@@ -1,5 +1,6 @@
 import requests
 import ujson
+import time
 
 def post_api(url, dic):
     rp = requests.post(url, json=dic)
@@ -9,3 +10,19 @@ def post_api(url, dic):
         return ujson.loads(rp.content)
     else:
         return {}
+
+def mpost_api_test(url, dic):
+    # Same tcp conn requests, quicker
+    now = time.time()
+    with requests.Session() as s:
+        for i in xrange(1,100):
+            s.post(url, json=dic).content
+    end = time.time()
+    print end-now
+
+    # Diff session request, 2x slower
+    now = time.time()
+    for i in xrange(1,100):
+        requests.post(url, json=dic)
+    end = time.time()
+    print end-now
