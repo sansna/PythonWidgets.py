c10a9bd0806688cd5977d849c1094cd36847de9b
sansna <1185280650@qq.com>
2020-04-14 15:55:00 +0800

    @Wentao[master]: update - sansna

--- fla.py
+++ fla.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/post", methods=["POST"])
+def post():
+    content=request.get_json(force=True)
+    print type(content), content["url"]
+    return content
 
 if __name__ == "__main__":
     app.run()
