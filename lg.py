74040fc5125b3686c7f504a77743589ef307c669
sansna <1185280650@qq.com>
2020-07-18 17:57:38 +0800

    @Wentao[master]: upd - sansna

--- lg.py
+++ lg.py
@@ -1,4 +1,9 @@
 import logging
 
-logging.basicConfig(filename="test.log", level=logging.INFO, format='%(asctime)s: %(levelname)s: %(filename)s:%(lineno)d: %(funcName)s')
-logging.info("ok")
+logging.basicConfig(filename="test.log", level=logging.INFO, format='%(asctime)s: %(levelname)s: %(filename)s:%(lineno)d: %(funcName)s: %(message)s')
+
+def main():
+    logging.info("ok")
+
+if __name__ == '__main__':
+    main()
