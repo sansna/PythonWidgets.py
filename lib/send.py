9443a9b762e7069a86b58696b794186523b815c0
sansna <1185280650@qq.com>
2020-08-01 17:34:50 +0800

    @Wentao[master]: upd send mail - sansna

---
+++ lib/send.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Author: sansna
+# Date  : 2020 Aug 01 17:32:31
+
+#import os
+#import sys 
+#sys.path.append(os.path.abspath("../../"))
+import time
+import yagmail as yg
+
+now = int(time.time())
+today = int(now+8*3600)/86400*86400-8*3600
+dayts = 86400
+hourts = 3600
+mints = 60
+yesterday = today - dayts
+
+def YMD(ts):
+    return time.strftime("%Y%m%d", time.localtime(ts))
+
+def YM(ts):
+    return time.strftime("%Y%m", time.localtime(ts))
+
+def DAY(ts):
+    return time.strftime("%d", time.localtime(ts))
+
+def InitUser():
+    # QQ mail example
+    a=yg.SMTP(user='',password='',host='smtp.exmail.qq.com')
+    return a
+
+def SendAsUser(obj,to,sub,att):
+    """
+    obj: user, from obj = InitUser()
+    to: "mail"
+    sub: "My sub"
+    att: ["file1", "file2"]
+    """
+    obj.send(to, subject=sub, attachments=att)
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
