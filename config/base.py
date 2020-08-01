04bee4fcc34ce265b178b8a4a8bb9dcd0e035b0b
sansna <1185280650@qq.com>
2020-08-02 00:04:13 +0800

    @Wentao[master]: upd - sansna

---
+++ config/base.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Author: sansna
+# Date  : 2020 Aug 01 22:25:36
+
+#import os
+#import sys 
+#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+import time
+
+now = int(time.time())
+today = int(now+8*3600)/86400*86400-8*3600
+dayts = 86400
+hourts = 3600
+mints = 60
+yesterday = today - dayts
+
+def YMD(ts):
+    return time.strftime("%Y%m%d", time.localtime(ts))
+
+def YM(ts):
+    return time.strftime("%Y%m", time.localtime(ts))
+
+def DAY(ts):
+    return time.strftime("%d", time.localtime(ts))
+
+Configured = False
+Env = "production"
+App = "myapp"
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
