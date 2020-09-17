f9dc5e08c0addfee4ac4a3a908b0e479945fe2b5
sansna <1185280650@qq.com>
2020-09-17 17:35:49 +0800

    @Wentao[master]: update iso8601 conver - sansna

--- t.py
+++ t.py
@@ -23,3 +23,14 @@ print d.year, d.month, d.day
 
 # weekday
 print d.weekday()
+
+# change between unixtimestamp & iso8601 format: 2006-01-02T15:04:05Z07:00
+## To iso8601
+import pytz
+tz = pytz.timezone('Asia/Shanghai')
+print datetime.datetime.fromtimestamp(1600334632, tz).isoformat()
+
+## To Unix timestamp
+import iso8601
+ts = int(time.mktime(iso8601.parse_date('2020-09-17T17:23:52+08:00').timetuple()))
+print ts
